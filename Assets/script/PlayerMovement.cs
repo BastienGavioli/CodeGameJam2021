ef106bad82baa3e30887cec60ba43c098bf567f9
Depeyre Gatien <gatien.depeyre@etu.umontpellier.com>
2021-01-18 23:33:51 +0100

    le jump est fait

--- Assets/script/PlayerMovement.cs
+++ Assets/script/PlayerMovement.cs
@@ -4,6 +4,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
 
+    private bool isJumping = false;
+
     public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
 
@@ -11,12 +13,23 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
         movePlayer(horizontalMovement);
+        isJumping = false;
     }
 
     void movePlayer(float _horizontalMovement)
     {
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
+
+        if (isJumping)
+        {
+            rb.AddForce(new Vector2(0f, 1000));
+        }
+
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
     }
 }
