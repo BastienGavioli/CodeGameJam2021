2afff4f77f9462e16d31a3084f33921b161bb0c0
Depeyre Gatien <gatien.depeyre@etu.umontpellier.com>
2021-01-19 10:01:04 +0100

    le menu commence

---
+++ Assets/script/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void lancerPartie()
+    {
+
+    }
+
+    public void ouvrirParametre()
+    {
+
+    }
+
+    public void quitter()
+    {
+        Application.Quit();
+    }
+}
