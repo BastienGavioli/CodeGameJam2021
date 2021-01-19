37f5071ab1b38b380e0cdc1363c0624ad9e7242f
Mélanie Fressard <fressardmelanie@gmail.com>
2021-01-19 20:22:48 +0100

    lvl0 complete

---
+++ Assets/script/PlayerOnlyRun.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerOnlyRun : MonoBehaviour
+{
+    public float moveSpeed;
+
+
+    public Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
+
+    public Animator animator;
+
+
+
+    void FixedUpdate()
+    {
+
+
+
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+
+
+        movePlayer(horizontalMovement);
+
+        float characterVelocity = Mathf.Abs(rb.velocity.x);
+        animator.SetFloat("Speed", characterVelocity);
+    }
+
+    void movePlayer(float _horizontalMovement)
+    {
+
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
+
+
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+    }
+
+}
