a937168c207e7eb15d20b5b27d8943ad14dc0530
Enzo Viguier <56982837+OniLinkExe@users.noreply.github.com>
2021-01-19 04:37:06 +0100

    Ajout du script qui permet le changement de scène

---
+++ Assets/script/changeScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour
+{
+
+    public string sceneNameToLoad;
+    void Start()
+    {
+        
+    }
+
+    
+    void loadThisScene()
+    {
+        SceneManager.LoadScene(sceneNameToLoad);
+    }
+
+}
