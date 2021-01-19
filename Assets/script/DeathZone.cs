011261d0e95b572375522ecb8d6022e9e5d8017b
Depeyre Gatien <gatien.depeyre@etu.umontpellier.com>
2021-01-19 15:21:31 +0100

    le perso meur correctement

--- Assets/script/DeathZone.cs
+++ Assets/script/DeathZone.cs
@@ -7,6 +7,7 @@ public class DeathZone : MonoBehaviour
 
     public life playerLife;
     public PlayerMovement movePlayer;
+    public PlayerSpawn spawn;
 
     void Start()
     {
@@ -21,13 +22,14 @@ public class DeathZone : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             playerLife.lostxLife(1);
-            //if (playerLife.estMort())
-            if(true)
+            if (playerLife.estMort())
+            //if(true)
             {
                 movePlayer.jumpHurt();
             }
             else
             {
+                spawn.replace();
                 //collision.transform.position = GameObject.FindGameObjectWithTag("PlayerSpawn").transform.position;
             }
 
--- Assets/script/PlayerSpawn.cs
+++ Assets/script/PlayerSpawn.cs
@@ -4,6 +4,12 @@ public class PlayerSpawn : MonoBehaviour
 {
    private void Awake()
     {
+        replace();
+    }
+
+    public void replace()
+    {
         GameObject.FindGameObjectWithTag("Player").transform.position = transform.position;
+
     }
 }
