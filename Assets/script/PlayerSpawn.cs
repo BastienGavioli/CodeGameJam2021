9a70babed6485abfc5c47af780d63e70c703bab1
Depeyre Gatien <gatien.depeyre@etu.umontpellier.com>
2021-01-19 15:37:49 +0100

    death zone fonctionnelle sur testCoin
    Attention le fond a disparu

--- Assets/script/PlayerSpawn.cs
+++ Assets/script/PlayerSpawn.cs
@@ -4,6 +4,10 @@ public class PlayerSpawn : MonoBehaviour
 {
    private void Awake()
     {
+        replace();
+    }
+    public void replace()
+    {
         GameObject.FindGameObjectWithTag("Player").transform.position = transform.position;
     }
 }
