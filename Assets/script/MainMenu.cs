645b0cd9d3d6cb59262f615cc00e673ee11e34e5
Mélanie Fressard <fressardmelanie@gmail.com>
2021-01-19 11:12:53 +0100

    menu principal

--- Assets/script/MainMenu.cs
+++ Assets/script/MainMenu.cs
@@ -4,21 +4,27 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public string Level;
+    public GameObject helpPanel;
 
     // Start is called before the first frame update
     void Start()
     {
-        SceneManager.LoadScene(Level);
+        
     }
 
     public void LoadGame()
     {
-
+        SceneManager.LoadScene(Level);
     }
 
     public void HelpWindowOpen()
     {
+        helpPanel.SetActive(true);
+    }
 
+    public void CloseHelp()
+    {
+        helpPanel.SetActive(false);
     }
 
     public void SettingsOppening()
