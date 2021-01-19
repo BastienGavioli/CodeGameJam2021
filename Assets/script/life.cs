035f3ca144f23a6f97482f91c0492efe3dab98b1
Depeyre Gatien <gatien.depeyre@etu.umontpellier.com>
2021-01-19 09:09:07 +0100

    systeme de pv fonctionnel

--- Assets/script/life.cs
+++ Assets/script/life.cs
@@ -4,23 +4,67 @@ using UnityEngine;
 
 public class life : MonoBehaviour
 {
-    public int vie = 3;
+    private int vie = 5;
     public GameObject coeur1;
     public GameObject coeur2;
     public GameObject coeur3;
     public GameObject coeur4;
     public GameObject coeur5;
 
+    private GameObject[] tabCoeur;
+
     // Start is called before the first frame update
     void Start()
     {
-        GameObject[] tabCoeur = { coeur1, coeur2, coeur3, coeur4, coeur5 };
-        coeur1.SetActive(false);
+        tabCoeur = new GameObject[] { coeur1, coeur2, coeur3, coeur4, coeur5 };
 
     }
 
-    // Update is called once per frame
+    // Update is called once per frame 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Il reste " + vie + "pv");
+            lostxLife(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Il reste " + vie + "pv");
+            gainxLife(1);
+        }
+    }
+
+    void lostxLife(int x)
+    {
+        vie -= 1;
+        if (vie < 0) { vie = 0; }
+        afficherVie();
+    }
+
+    void gainxLife(int x)
+    {
+        vie += x;
+        if (vie > 5) { vie = 5; }
+        afficherVie();
+
+    }
+
+    void viderCoeurs()
+    {
+        foreach (GameObject coeur in tabCoeur)
+        {
+            coeur.SetActive(false);
+        }
+    }
+
+    void afficherVie()
+    {
+        viderCoeurs();
+        for(int i=0; i<vie; i++)
+        {
+            tabCoeur[i].SetActive(true);
+
+        }
     }
 }
