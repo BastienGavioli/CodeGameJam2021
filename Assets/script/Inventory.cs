898e19045d259a6c449e1d26292de545b7493866
BastienGavioli <bastien.gavioli34@gmail.com>
2021-01-19 10:13:54 +0100

    CoinScriptPrototype

---
+++ Assets/script/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public int coinsCount;
+
+    public static Inventory instance;
+
+    public void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance d'Inventory dans la scène");
+            return;
+        }
+        
+        instance = this;
+    }
+
+    public void AddCoins(int count)
+    {
+        coinsCount += count;
+    }
+}
