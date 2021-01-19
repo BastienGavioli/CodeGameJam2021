1f669d73f9c1ac72e177610f4a955be5b7f5efa9
Mélanie Fressard <fressardmelanie@gmail.com>
2021-01-19 10:49:04 +0100

    font + main menu

--- Assets/script/MainMenu.cs
+++ Assets/script/MainMenu.cs
@@ -1,26 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public string Level;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SceneManager.LoadScene(Level);
     }
 
-    public void lancerPartie()
+    public void LoadGame()
     {
 
     }
 
-    public void ouvrirParametre()
+    public void HelpWindowOpen()
     {
 
     }
 
-    public void quitter()
+    public void SettingsOppening()
+    {
+
+    }
+
+    public void QuitGame()
     {
         Application.Quit();
     }
