6dcbe605336a5b1a2807a7689abcae8b7b6980d6
Mélanie Fressard <fressardmelanie@gmail.com>
2021-01-20 07:50:11 +0100

    ecran fin fonctionnel

---
+++ Assets/AffichagePanel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AffichagePanel : MonoBehaviour
+{
+    public GameObject lePanel;
+    public string leMenu;
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(leMenu);
+    }
+
+    public void Affichage()
+    {
+        lePanel.SetActive(true);
+    }
+}
